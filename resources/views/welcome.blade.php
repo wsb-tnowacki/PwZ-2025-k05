bf8fca2e7fe02ab0795a76016430f494608ce02c
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2025-03-29 14:36:35 +0100

    Dodanie autoryzacji Breeze Blade oraz poprawienie routingu i widoków

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.layout')
+@section('tytul', 'Strona startowa')
+@section('podtytul', 'Strona startowa')
+
+@section('tresc')
+    <div>
+        Treść strony startowej 
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae velit repellat voluptatibus, vel aliquid eveniet exercitationem quod? Voluptas, eaque id molestiae itaque, est, exercitationem culpa voluptatum expedita voluptate impedit ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam necessitatibus possimus alias optio sed, consequuntur fugiat impedit aperiam doloribus cumque corrupti modi odio consectetur vitae, minus incidunt dolore eius veritatis!</div>
+    </div>
+@endsection
